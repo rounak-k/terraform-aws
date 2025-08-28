44e91b8c362db7be1e9c187221f626a086529e8e
rounak-sanofi <Rounak.Kumar@sanofi.com>
2025-08-28 13:13:17 +0530

    adding new ec2 instances

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+       version = "6.10.0"
+    }
+  }
+}
 provider "aws" {
   region = "ap-south-1"
 }
@@ -5,6 +13,7 @@ provider "aws" {
 resource "aws_security_group" "ec2_sg" {
   name = "ec2-sg"
   description = "Allow SSH and HTTP"
+  
 
   ingress {
     from_port = 22
@@ -29,6 +38,7 @@ resource "aws_security_group" "ec2_sg" {
 }
 resource "aws_instance" "my_ec2" {
   ami = "ami-0861f4e788f5069dd"
+  count = 2
   instance_type = "t2.micro"
   key_name ="DemoKeyPair" 
   security_groups = [aws_security_group.ec2_sg.name]
@@ -39,5 +49,5 @@ resource "aws_instance" "my_ec2" {
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+  value = aws_instance.my_ec2[*].public_ip
 }
